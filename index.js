64f83d425be29f39941eaa5f3e966503956b2ac7
Robert Mariuson <gigglefeet@gmail.com>
2021-01-19 18:41:36 +0100

    Add final changes

---
+++ index.js
@@ -0,0 +1,15 @@
+const randomColor = require('randomcolor'); // import the script
+const chalk = require('chalk'); // import chalk
+const color = randomColor({
+  luminosity: process.argv[2],
+  hue: process.argv[3],
+}); // a hex code for an attractive color
+console.log(chalk.hex(color)('###############################'));
+console.log(chalk.hex(color)('###############################'));
+console.log(chalk.hex(color)('###############################'));
+console.log(chalk.hex(color)('#####                     #####'));
+console.log(chalk.hex(color)('#####       ' + color + '       #####'));
+console.log(chalk.hex(color)('#####                     #####'));
+console.log(chalk.hex(color)('###############################'));
+console.log(chalk.hex(color)('###############################'));
+console.log(chalk.hex(color)('###############################'));
