d2c70d3f747db5a12fc9d81343a106eb15c2deda
Robert Mariuson <gigglefeet@gmail.com>
2021-01-25 12:48:02 +0100

    Add Joses suggestions to final version. Now finished!

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const randomColor = require('randomcolor'); // import the script
 const chalk = require('chalk'); // import chalk
 const color = randomColor({
-  luminosity: process.argv[2],
   hue: process.argv[3],
+  luminosity: process.argv[2],
 }); // a hex code for an attractive color
 console.log(chalk.hex(color)('###############################'));
 console.log(chalk.hex(color)('###############################'));
