07eb599e633a59912dba339dcf60dcf50e6119f9
Robert Mariuson <gigglefeet@gmail.com>
2021-01-26 14:16:46 +0100

    Joses suggestions to final version. Now finished! 2.0.0

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Command line random-color-generator
 
 Simple node.js color generator
+To use it, one has to simply type in the terminal/command line the following
+"node index.js"
+to get a specific color you can type:
+"node index.js red"
+to get a random red color and to get a random red or any other color with specific luminosity type:
+"node index.js green dark"
+it is also possible to use "light" in stead of dark and then the color of your choosing or just skip the color command and use light or dark to get light and dark luminosity versions of random colors.
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const randomColor = require('randomcolor'); // import the script
 const chalk = require('chalk'); // import chalk
 const color = randomColor({
-  hue: process.argv[3],
-  luminosity: process.argv[2],
+  hue: process.argv[2],
+  luminosity: process.argv[3],
 }); // a hex code for an attractive color
 console.log(chalk.hex(color)('###############################'));
 console.log(chalk.hex(color)('###############################'));
@@ -13,3 +13,4 @@ console.log(chalk.hex(color)('#####                     #####'));
 console.log(chalk.hex(color)('###############################'));
 console.log(chalk.hex(color)('###############################'));
 console.log(chalk.hex(color)('###############################'));
+/* to activate the random color generator, type "node index.js" for more instructions read the README.MD*/
